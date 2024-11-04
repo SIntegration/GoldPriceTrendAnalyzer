4fe4e4d3386367f1a1c1dcdb988562d49d2ca604
Sascha <arkankraft@outlook.de>
2024-11-04 18:01:29 +0100

    Main Code implementiert

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+         
+            static async Task Main(string[] args)
+            {
+                var dataService = new MarketDataService();
+                var forecast = new GoldTrendForecast();
+
+                try
+                {
+                    var marketData = await dataService.GetMarketDataAsync();
+                    forecast.GenerateForecast(marketData);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Fehler beim Abrufen der Marktdaten: " + ex.Message);
+                }
+            }
         }
     }
 }
+
